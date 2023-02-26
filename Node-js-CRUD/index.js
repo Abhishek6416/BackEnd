ed97e9481aa132c492d98a5849d3e7812236c2a6
Abhishek6416 <abhishekkr6416@gmail.com>
2023-02-26 08:31:31 +0530

    nodejs-CRUD

---
+++ Node-js-CRUD/index.js
@@ -0,0 +1,25 @@
+const fs=require("fs")
+
+// fs.mkdir("abk",(err)=>{
+//     console.log("folder created")
+// })
+
+fs.writeFile("bio.txt","my name is yo yo",(err)=>{
+    console.log("files created")
+})
+
+fs.appendFile("bio.txt","anjali mondalllllllllllllllllllll",(err)=>{
+    console.log(" new data added")
+})
+
+fs.readFile("bio.txt","utf-8",(err,data)=>{
+    console.log(data);
+})
+
+fs.rename("bio.txt","anjali.txt",(err)=>{
+    console.log("file renamed")
+})
+
+fs.unlink("anjali.txt",(err)=>{
+    console.log("file deleted")
+})
